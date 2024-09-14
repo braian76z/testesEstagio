64e372416f1acb40bb036c7ab53cd815d47ed767
Braian Mauricio <braiantrab@gmail.com>
2024-09-14 19:24:08 -0300

    Add files via upload

---
+++ teste5.php
@@ -0,0 +1,9 @@
+<?php
+// Explicação do problema dos interruptores
+echo "1. Ligue o primeiro interruptor e deixe ligado por alguns minutos.\n";
+echo "2. Desligue o primeiro interruptor e ligue o segundo interruptor.\n";
+echo "3. Vá até a sala das lâmpadas:\n";
+echo "- A lâmpada acesa é controlada pelo segundo interruptor.\n";
+echo "- A lâmpada apagada, mas quente, é controlada pelo primeiro interruptor.\n";
+echo "- A lâmpada apagada e fria é controlada pelo terceiro interruptor.\n";
+?>
